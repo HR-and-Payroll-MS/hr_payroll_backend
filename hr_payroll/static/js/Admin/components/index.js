c72917b313eda845e337fcb5d2400e6a6ff60ebd
Seud <seudsahm1@gmail.com>
2025-12-05 18:11:22 +0300

    revert: Remove React admin theme, keep simple green header color only

--- hr_payroll/static/js/Admin/components/index.js
+++
@@ -1,5 +0,0 @@
-// Export all components for easy importing
-export { default as Card } from './Card';
-export { default as Button } from './Button';
-export { default as Table } from './Table';
-export { default as Input } from './Form/Input';
