deefc1e61bf212aaf2f31f73f6b046b6bc924405
Seud <seudsahm1@gmail.com>
2025-12-22 11:38:33 +0300

    fix: reduce ACCESS_TOKEN_LIFETIME to 15 minutes for enhanced security

--- config/settings/base.py
+++ config/settings/base.py
@@ -407,7 +407,7 @@ REST_AUTH = {
 
 # SimpleJWT defaults
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
